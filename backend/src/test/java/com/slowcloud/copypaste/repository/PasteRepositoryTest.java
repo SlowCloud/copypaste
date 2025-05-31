d4165db07ed2c3ee4c7bf75f7bc87c09ebcbfa27
slowcloud <sju9417@gmail.com>
2025-05-31 20:02:03 +0900

    test: Repository 테스트 코드 작성 및 테스트 properties 작성

---
+++ backend/src/test/java/com/slowcloud/copypaste/repository/PasteRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.slowcloud.copypaste.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class PasteRepositoryTest {
+
+    @Autowired
+    PasteRepository pasteRepository;
+
+    @Test
+    void instanceCheck() {
+        assertNotNull(pasteRepository);
+    }
+
+}
