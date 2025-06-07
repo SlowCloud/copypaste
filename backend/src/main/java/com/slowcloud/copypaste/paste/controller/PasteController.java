40e0fcbe4a44be814bfc035b6ab907c05e8e100e
slowcloud <sju9417@gmail.com>
2025-06-07 20:22:54 +0900

    feat: Paste를 리스트로 불러오는 API 구현

--- backend/src/main/java/com/slowcloud/copypaste/paste/controller/PasteController.java
+++ backend/src/main/java/com/slowcloud/copypaste/paste/controller/PasteController.java
@@ -11,6 +11,11 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -32,6 +38,12 @@ public class PasteController {
     public ResponseEntity<PasteGetResponse> getPasteByPasteId(@PathVariable @Min(1) long pasteId) {
         PasteGetResponse pasteResponseDto = pasteService.getPasteFromPasteId(pasteId);
         return ResponseEntity.ok(pasteResponseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PasteGetResponse>> getMethodName(@PageableDefault(sort = "id") Pageable pageable) {
+        List<PasteGetResponse> pagePasteResponseDto = pasteService.getPastes(pageable);
+        return ResponseEntity.ok(pagePasteResponseDto);
     }
 
     @PostMapping
--- backend/src/main/java/com/slowcloud/copypaste/paste/service/PasteService.java
+++ backend/src/main/java/com/slowcloud/copypaste/paste/service/PasteService.java
@@ -1,5 +1,9 @@
 package com.slowcloud.copypaste.paste.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.slowcloud.copypaste.paste.dto.PasteCreateRequest;
@@ -18,11 +22,7 @@ public class PasteService {
 
     public PasteGetResponse getPasteFromPasteId(long pasteId) {
         Paste paste = pasteRepository.findById(pasteId).orElseThrow();
-        return PasteGetResponse.builder()
-            .id(paste.getId())
-            .content(paste.getContent())
-            .syntaxHighlight(paste.getSyntaxHighlight())
-            .build();
+        return pasteToPasteGetResponse(paste);
     }
 
     public PasteCreateResponse createPaste(PasteCreateRequest pasteCreateRequest) {
@@ -33,6 +33,21 @@ public class PasteService {
         Paste createdPaste = pasteRepository.save(paste);
         return PasteCreateResponse.builder()
             .pasteId(createdPaste.getId())
+            .build();
+    }
+
+    public List<PasteGetResponse> getPastes(Pageable pageable) {
+        return pasteRepository
+            .findAll(pageable)
+            .map(PasteService::pasteToPasteGetResponse)
+            .getContent();
+    }
+
+    private static PasteGetResponse pasteToPasteGetResponse(Paste paste) {
+        return PasteGetResponse.builder()
+            .id(paste.getId())
+            .content(paste.getContent())
+            .syntaxHighlight(paste.getSyntaxHighlight())
             .build();
     }
 
