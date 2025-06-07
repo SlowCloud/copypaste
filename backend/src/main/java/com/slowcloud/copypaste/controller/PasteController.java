7736e68ae71790e59c14dfee9b22a547148e97b7
slowcloud <sju9417@gmail.com>
2025-06-07 19:26:50 +0900

    feat: DTO validation 설정

--- backend/src/main/java/com/slowcloud/copypaste/controller/PasteController.java
+++ backend/src/main/java/com/slowcloud/copypaste/controller/PasteController.java
@@ -7,18 +7,18 @@ import com.slowcloud.copypaste.dto.PasteCreateResponse;
 import com.slowcloud.copypaste.dto.PasteGetResponse;
 import com.slowcloud.copypaste.service.PasteService;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 
 @RequiredArgsConstructor
 @RestController
@@ -28,13 +28,14 @@ public class PasteController {
     private final PasteService pasteService;
 
     @GetMapping("/{pasteId}")
-    public ResponseEntity<PasteGetResponse> getPasteByPasteId(@PathVariable long pasteId) {
+    @Validated
+    public ResponseEntity<PasteGetResponse> getPasteByPasteId(@PathVariable @Min(1) long pasteId) {
         PasteGetResponse pasteResponseDto = pasteService.getPasteFromPasteId(pasteId);
         return ResponseEntity.ok(pasteResponseDto);
     }
 
     @PostMapping
-    public ResponseEntity<PasteCreateResponse> createPaste(@RequestBody PasteCreateRequest pasteCreateRequest) {
+    public ResponseEntity<PasteCreateResponse> createPaste(@RequestBody @Valid PasteCreateRequest pasteCreateRequest) {
         PasteCreateResponse pasteCreateResponse = pasteService.createPaste(pasteCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(pasteCreateResponse);
     }
--- backend/src/main/java/com/slowcloud/copypaste/dto/PasteCreateRequest.java
+++ backend/src/main/java/com/slowcloud/copypaste/dto/PasteCreateRequest.java
@@ -2,12 +2,16 @@ package com.slowcloud.copypaste.dto;
 
 import com.slowcloud.copypaste.entity.SyntaxHighlight;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
 
 @Builder
 @Value
 public class PasteCreateRequest {
+    @Max(value = 10000L, message = "텍스트 길이 제한을 초과하였습니다.")
     private final String content;
+    @NotNull
     private final SyntaxHighlight syntaxHighlight;
 }
