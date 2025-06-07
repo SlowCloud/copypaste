1cfc8ee6a2250fdb054b5ee5f1099bd645d2f8de
slowcloud <sju9417@gmail.com>
2025-06-07 20:24:57 +0900

    chore: import 최적화

--- backend/src/main/java/com/slowcloud/copypaste/paste/controller/PasteController.java
+++ backend/src/main/java/com/slowcloud/copypaste/paste/controller/PasteController.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
